9abbe96a5ffaa7d804b1d540c7e05640f9089f3b
bagas-sw <bagassw42@gmail.com>
2022-09-14 22:40:49 +0700

    Update Katalog App

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
+from multiprocessing import context
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog' : data_katalog,
+        'nama'  : 'Bagas Shalahuddin Wahid',
+        'npm'   : '2106708904'
+    }
+    return render(request, "katalog.html", context)
